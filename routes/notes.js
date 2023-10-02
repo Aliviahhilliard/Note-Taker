f3ed01bc6b067640fe43254b2c4288ecc19b49be
Aliviah Hilliard <aliviahhilliard@gmail.com>
2023-10-02 16:39:12 -0400

    Added comments and fixed code structure.

--- routes/notes.js
+++ routes/notes.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-const { readFromFile, writeToFile, readAndAppend } = require('./fsUtils');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils'); // adjust the path as needed
+
 const router = express.Router();
 
 // Serve the notes.html file
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
+  const filePath = path.join(__dirname, '../public/notes.html');  
+  res.sendFile(filePath); 
+});  // Added missing closing parenthesis
 
 // Fetch notes
 router.get('/api/notes', async (req, res) => {
   try {
-    const data = await readFromFile('db/notes.json');
+    const data = await readFromFile('../db/notes.json'); // adjust the path as needed
     return res.json(JSON.parse(data));
   } catch (err) {
     res.status(500).send("Internal Server Error");
@@ -26,7 +28,7 @@ router.post('/api/notes', (req, res) => {
     text: req.body.text,
     id: uuidv4(),
   };
-  readAndAppend(newNote, 'db/notes.json');
+  readAndAppend(newNote, '../db/notes.json'); // adjust the path as needed
   res.json(newNote);
 });
 
@@ -34,10 +36,10 @@ router.post('/api/notes', (req, res) => {
 router.delete('/api/notes/:id', async (req, res) => {
   const noteId = req.params.id;
   try {
-    const data = await readFromFile('db/notes.json');
+    const data = await readFromFile('../db/notes.json'); // adjust the path as needed
     let notes = JSON.parse(data);
     notes = notes.filter(note => note.id !== noteId);
-    writeToFile('db/notes.json', notes);
+    writeToFile('../db/notes.json', notes); // adjust the path as needed
     res.status(200).send("Note deleted");
   } catch (err) {
     res.status(500).send("Internal Server Error");
